14a29039a367e4f0e34b17edc5985b178ca2b906
00009474 <72804600+00009474@users.noreply.github.com>
2020-11-04 11:32:03 +0500

    Added tasks

---
+++ task4.py
@@ -0,0 +1,11 @@
+
+def value(x, y):
+    if x > y:
+        return 1
+    elif x == y:
+        return 0
+    elif x < y:
+        return -1
+print(value(1, 0))
+print(value(1, 1))
+print(value(4, 6))
